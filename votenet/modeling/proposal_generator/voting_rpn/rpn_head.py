ba79e55f18958d2db4b50962a4ab3e51569227fd
Vivym <1013114323@qq.com>
2020-11-17 05:03:00 +0800

    convs -> cls_convs + reg_convs

--- votenet/modeling/proposal_generator/voting_rpn/rpn_head.py
+++ votenet/modeling/proposal_generator/voting_rpn/rpn_head.py
@@ -123,3 +123,131 @@ class StandardRPNHead(nn.Module):
             pred_centerness = self.centerness_predictor(x).squeeze(1)
 
         return pred_objectness_logits, pred_box_reg, pred_heading_cls_logits, pred_heading_deltas, pred_centerness
+
+
+@RPN_HEAD_REGISTRY.register()
+class StandardRPNHead2(nn.Module):
+    """
+    Standard RPN classification and regression heads described in :paper:`Faster R-CNN`.
+    Uses a 3x3 conv to produce a shared hidden state from which one 1x1 conv predicts
+    objectness logits for each anchor and a second 1x1 conv predicts bounding-box deltas
+    specifying how to deform each anchor into an object proposal.
+    """
+
+    @configurable
+    def __init__(
+            self, *,
+            use_axis_aligned_box: bool,
+            use_centerness: bool,
+            use_exp: bool,
+            objectness_loss_type: str,
+            fuse_xyz: int,
+            fuse_xyz_type: str,
+    ):
+        super().__init__()
+
+        self.use_axis_aligned_box = use_axis_aligned_box
+        self.use_centerness = use_centerness
+        self.use_exp = use_exp
+        self.objectness_loss_type = objectness_loss_type
+        self.fuse_xyz = fuse_xyz
+        self.fuse_xyz_type = fuse_xyz_type
+
+        if fuse_xyz > 0:
+            self.positional_encoding = build_positional_encoding_layer(
+                fuse_xyz_type, dim=fuse_xyz, fuse_type="cat"
+            )
+            num_extra_channels = self.positional_encoding.num_extra_channels
+        else:
+            num_extra_channels = 0
+
+        cls_convs = [
+            nn.Conv1d(128 + num_extra_channels, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+        ]
+        self.cls_convs = nn.Sequential(*cls_convs)
+
+        reg_convs = [
+            nn.Conv1d(128 + num_extra_channels, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+        ]
+        self.reg_convs = nn.Sequential(*reg_convs)
+
+        self.objectness_predictor = nn.Conv1d(
+            128, 2 if objectness_loss_type == "cross_entropy" else 1,
+            kernel_size=1,
+        )
+        self.box_predictor = nn.Conv1d(128, 6, kernel_size=1)
+        predictors = [self.objectness_predictor, self.box_predictor]
+        # TODO:
+        assert use_axis_aligned_box
+        if use_centerness:
+            self.centerness_predictor = nn.Conv1d(128, 1, kernel_size=1)
+            predictors.append(self.centerness_predictor)
+
+        for layer in cls_convs + reg_convs:
+            if isinstance(layer, nn.Conv1d):
+                weight_init.c2_msra_fill(layer)
+
+        for predictor in predictors:
+            nn.init.normal_(predictor.weight, std=0.001)
+            if predictor.bias is not None:
+                nn.init.constant_(predictor.bias, 0)
+
+    @classmethod
+    def from_config(cls, cfg):
+        return {
+            "use_axis_aligned_box": cfg.INPUT.AXIS_ALIGNED_BOX,
+            "use_centerness": cfg.MODEL.RPN.CENTERNESS,
+            "use_exp": cfg.MODEL.RPN.USE_EXP,
+            "objectness_loss_type": cfg.MODEL.RPN.OBJECTNESS_LOSS_TYPE,
+            "fuse_xyz": cfg.MODEL.RPN.FUSE_XYZ,
+            "fuse_xyz_type": cfg.MODEL.RPN.FUSE_XYZ_TYPE,
+        }
+
+    def forward(self, xyz: torch.Tensor, features: torch.Tensor):
+        """
+        Args:
+            xyz(Tensor): xyz
+            features (Tensor): feature map
+
+        Returns:
+            list[Tensor]: A list of L elements.
+                Element i is a tensor of shape (N, A, Hi, Wi) representing
+                the predicted objectness logits for all anchors. A is the number of cell anchors.
+            list[Tensor]: A list of L elements. Element i is a tensor of shape
+                (N, A*box_dim, Hi, Wi) representing the predicted "deltas" used to transform anchors
+                to proposals.
+        """
+        if self.fuse_xyz > 0:
+            features = self.positional_encoding(xyz.permute(0, 2, 1), features)
+
+        cls_features = self.cls_convs(features)
+        reg_features = self.reg_convs(features)
+
+        # (bs, num_proposals, 2)
+        pred_objectness_logits = self.objectness_predictor(cls_features).permute(0, 2, 1)
+        # (bs, num_proposals, 6)
+        pred_box_reg = self.box_predictor(reg_features).permute(0, 2, 1)
+        if self.use_exp:
+            pred_box_reg = pred_box_reg.exp()
+
+        pred_heading_cls_logits, pred_heading_deltas = None, None
+
+        # TODO:
+        assert self.use_axis_aligned_box
+
+        pred_centerness = None
+        if self.use_centerness:
+            # (bs, num_proposals)
+            pred_centerness = self.centerness_predictor(reg_features).squeeze(1)
+
+        return pred_objectness_logits, pred_box_reg, pred_heading_cls_logits, pred_heading_deltas, pred_centerness
--- votenet/modeling/roi_heads/box_head.py
+++ votenet/modeling/roi_heads/box_head.py
@@ -127,3 +127,121 @@ class StandardBoxHead(nn.Module):
             pred_centerness = self.centerness_predictor(x).view(batch_size, num_proposals)
 
         return pred_cls_logits, pred_box_deltas, pred_heading_deltas, pred_centerness
+
+
+@ROI_BOX_HEAD_REGISTRY.register()
+class StandardBoxHead2(nn.Module):
+    """
+    A head with several 3x3 conv layers (each followed by norm & relu) and then
+    several fc layers (each followed by relu).
+    """
+
+    @configurable
+    def __init__(
+            self, *,
+            num_classes: int,
+            use_axis_aligned_box: bool,
+            use_centerness: bool,
+            use_exp: bool,
+            cls_agnostic_bbox_reg: bool,
+            cls_loss_type: str,
+            prior_prob: float,
+    ):
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.use_axis_aligned_box = use_axis_aligned_box
+        self.use_centerness = use_centerness
+        self.use_exp = use_exp
+        self.cls_agnostic_bbox_reg = cls_agnostic_bbox_reg
+        self.cls_loss_type = cls_loss_type
+
+        cls_convs = [
+            nn.Conv1d(128 + 128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+        ]
+        self.cls_convs = nn.Sequential(*cls_convs)
+
+        reg_convs = [
+            nn.Conv1d(128 + 128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 128, kernel_size=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+        ]
+        self.reg_convs = nn.Sequential(*reg_convs)
+
+        self.cls_predictor = nn.Conv1d(
+            128, num_classes + (1 if cls_loss_type == "cross_entropy" else 0),
+            kernel_size=1,
+        )
+        if cls_agnostic_bbox_reg:
+            self.box_predictor = nn.Conv1d(128, 6, kernel_size=1)
+        else:
+            self.box_predictor = nn.Conv1d(128, num_classes * 6, kernel_size=1)
+        predictors = [self.cls_predictor, self.box_predictor]
+        if not use_axis_aligned_box:
+            self.box_angle_predictor = nn.Conv1d(128, num_classes * 1, kernel_size=1)
+            predictors.append(self.box_angle_predictor)
+        if use_centerness:
+            self.centerness_predictor = nn.Conv1d(128, 1, kernel_size=1)
+            predictors.append(self.centerness_predictor)
+
+        for layer in cls_convs + reg_convs:
+            if isinstance(layer, nn.Conv1d):
+                weight_init.c2_msra_fill(layer)
+
+        for predictor in predictors:
+            nn.init.normal_(predictor.weight, std=0.001)
+            if predictor.bias is not None:
+                nn.init.constant_(predictor.bias, 0)
+
+        if cls_loss_type == "sigmoid_focal_loss":
+            # Use prior in model initialization to improve stability
+            bias_value = -(math.log((1 - prior_prob) / prior_prob))
+            nn.init.constant_(self.cls_predictor.bias, bias_value)
+
+    @classmethod
+    def from_config(cls, cfg):
+        return {
+            "num_classes": cfg.MODEL.ROI_HEADS.NUM_CLASSES,
+            "use_axis_aligned_box": cfg.INPUT.AXIS_ALIGNED_BOX,
+            "use_centerness": cfg.MODEL.ROI_BOX_HEAD.CENTERNESS,
+            "use_exp": cfg.MODEL.ROI_BOX_HEAD.USE_EXP,
+            "cls_agnostic_bbox_reg": cfg.MODEL.ROI_BOX_HEAD.CLS_AGNOSTIC_BBOX_REG,
+            "cls_loss_type": cfg.MODEL.ROI_BOX_HEAD.CLS_LOSS_TYPE,
+            "prior_prob": cfg.MODEL.ROI_BOX_HEAD.PRIOR_PROB,
+        }
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        num_proposals = x.size(-1)
+
+        cls_features = self.cls_convs(x)
+        reg_features = self.reg_convs(x)
+
+        pred_cls_logits = self.cls_predictor(cls_features).permute(0, 2, 1)
+        pred_box_deltas = self.box_predictor(reg_features).permute(0, 2, 1)
+        if not self.cls_agnostic_bbox_reg:
+            pred_box_deltas = pred_box_deltas.view(
+                batch_size, num_proposals, self.num_classes, 6
+            )  # (bs, num_proposals, num_classes, 6)
+        if self.use_exp:
+            pred_box_deltas = pred_box_deltas.exp()
+
+        pred_heading_deltas = None
+        if not self.use_axis_aligned_box:
+            # (bs, num_proposals, num_classes)
+            pred_heading_deltas = self.box_angle_predictor(reg_features).permute(0, 2, 1)
+
+        pred_centerness = None
+        if self.use_centerness:
+            # (bs, num_proposals)
+            pred_centerness = self.centerness_predictor(reg_features).view(batch_size, num_proposals)
+
+        return pred_cls_logits, pred_box_deltas, pred_heading_deltas, pred_centerness
