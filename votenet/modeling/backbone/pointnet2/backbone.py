eab0833fac8afa30a160a5162c81d42cb271c733
Vivym <1013114323@qq.com>
2021-01-20 22:03:44 +0800

    bugfix: multi backbone

--- votenet/modeling/backbone/pointnet2/backbone.py
+++ votenet/modeling/backbone/pointnet2/backbone.py
@@ -116,6 +116,7 @@ class PointNet2(Backbone):
         outputs["sa3"] = {
             "xyz": xyz,
             "features": features,
+            "inds": fps_inds,
         }
 
         xyz, features, fps_inds = self.sa4(
@@ -124,6 +125,7 @@ class PointNet2(Backbone):
         outputs["sa4"] = {
             "xyz": xyz,
             "features": features,
+            "inds": fps_inds,
         }
 
         # --------- 2 FEATURE UPSAMPLING LAYERS --------
@@ -153,7 +155,7 @@ def build_pointnet2_backbone(cfg):
     return PointNet2(int(use_color) * 3 + int(use_height) * 1)
 
 
-class MultiBackbone(nn.Module):
+class MultiBackbone(Backbone):
     def __init__(self, input_feature_dim):
         super().__init__()
         self.backbones = nn.ModuleList((
@@ -169,9 +171,20 @@ class MultiBackbone(nn.Module):
         self.bn_agg2 = torch.nn.BatchNorm1d(256)
 
     def forward(self, x: torch.Tensor):
-        features = self.backbones(x)
-        xyz = features[0]["fp2"]["xyz"]
-        inds = features[0]["fp2"]["inds"].long()
+        features0 = self.backbones[0](x)
+        sa1_inds = features0["sa1"]["inds"]
+        sa2_inds = features0["sa2"]["inds"]
+        sa3_inds = features0["sa3"]["inds"]
+        sa4_inds = features0["sa4"]["inds"]
+        features = [features0]
+        for backbone in self.backbones[1:]:
+            features_i = backbone(
+                x, sa1_inds=sa1_inds, sa2_inds=sa2_inds, sa3_inds=sa3_inds, sa4_inds=sa4_inds
+            )
+            features.append(features_i)
+
+        xyz = features0["fp2"]["xyz"]
+        inds = features0["fp2"]["inds"].long()
 
         features = torch.cat((
             features[0]["fp2"]["features"],
@@ -196,4 +209,4 @@ def build_multi_pointnet2_backbone(cfg):
     use_color = cfg.INPUT.USE_COLOR
     use_height = cfg.INPUT.USE_HEIGHT
 
-    return PointNet2(int(use_color) * 3 + int(use_height) * 1)
+    return MultiBackbone(int(use_color) * 3 + int(use_height) * 1)
